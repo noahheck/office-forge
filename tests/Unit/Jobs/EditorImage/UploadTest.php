e130c5b9cb9911bb53621c2e86518685ddd8a594
Noah Heck <noah@noahheck.com>
2019-12-03 17:08:41 -0700

    Add tests for EditorImage/Upload
    Closes #22

---
+++ tests/Unit/Jobs/EditorImage/UploadTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Unit\Jobs\EditorImage;
+
+use App\Jobs\EditorImage\Upload;
+use App\Project;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\UploadedFile;
+use Tests\TestCase;
+
+class UploadTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testUploadRecordsEntryInDatabaseAndStoresFileOnDisk()
+    {
+        Storage::fake();
+
+        $user = factory(User::class)->create();
+
+        $file = UploadedFile::fake()->image('headshot.jpg');
+
+        $job = new Upload(
+            $file,
+            Project::class,
+            null,
+            'abc123',
+            $user
+        );
+
+        app()->call([$job, 'handle']);
+
+        $editorImage = $job->getEditorImage();
+
+        $path = "/editor-images/" . $editorImage->filename;
+
+        Storage::assertExists($path);
+
+        $this->assertEquals('image/jpeg', $editorImage->type);
+        $this->assertEquals('headshot.jpg', $editorImage->original_filename);
+        $this->assertEquals($user->id, $editorImage->uploaded_by);
+        $this->assertEquals('abc123', $editorImage->resource_temp_id);
+    }
+}
