b9a17f5e7986385a29d2243f0f40cd6ae0661b82
Noah Heck <noah@noahheck.com>
2019-12-13 14:00:57 -0700

    Make sure the overdue logic is complete
    Closes #32

--- app/Traits/HasDueDate.php
+++ app/Traits/HasDueDate.php
@@ -12,6 +12,10 @@ trait HasDueDate
             return null;
         }
 
+        if ($this->completed) {
+            return false;
+        }
+
         $today = \Auth::user()->today();
 
         $dueDate = $this->due_date->setTime(23, 59, 59)->setTimezone($today->timezone);
@@ -25,9 +29,16 @@ trait HasDueDate
             return null;
         }
 
+        if ($this->completed) {
+            return false;
+        }
+
         $today = \Auth::user()->today();
 
-        $dueDate = $this->due_date->setTime(23, 59, 59)->setTimezone($today->timezone);
+        $dueDate = $this->due_date->setTime(12, 00, 00)->setTimezone($today->timezone)->setTime(23, 59, 59);
+
+        \Debugbar::info($today);
+        \Debugbar::info($dueDate);
 
         return $dueDate->isSameDay($today);
     }
