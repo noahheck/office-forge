c6f0caca0847cb9f6bfc70149b1ac207f4549ffc
Noah Heck <noah@noahheck.com>
2020-01-09 16:42:48 -0700

    Reorganize Process show view
    Also, closes #50

--- resources/views/admin/processes/show.blade.php
+++ resources/views/admin/processes/show.blade.php
@@ -8,25 +8,34 @@
 ])
 
 @section('content')
-    <h1>
-        <span class="fas fa-clipboard-list"></span>
-        {{ $process->name }}
-    </h1>
 
-    <div class="card">
-        <div class="card-body">
 
-            <div class="row">
+    <div class="row justify-content-center">
 
-                <div class="col-12 col-md-3 order-1 order-md-2">
+        <div class="col-12 col-md-8">
 
-                    <a href="{{ route('admin.processes.edit', [$process]) }}" class="btn btn-primary">
-                        <span class="fas fa-edit"></span> {{ __('admin.editProcess') }}
-                    </a>
+            <div class="card">
+
+                <div class="card-body">
+
+                    <h1 class="h3">
+                        <span class="fas fa-clipboard-list"></span>
+                        {{ $process->name }}
+                    </h1>
 
                     <hr>
 
-                    <span class="far fa-{{ $process->active ?? false ? 'check-' : '' }}square"></span> {{ __('process.active') }}
+                    <div class="d-flex justify-content-between">
+
+                        <span>
+                            <span class="far fa-{{ $process->active ?? false ? 'check-' : '' }}square"></span> {{ __('process.active') }}
+                        </span>
+
+                        <a href="{{ route('admin.processes.edit', [$process]) }}" class="btn btn-sm btn-primary">
+                            <span class="fas fa-edit"></span> {{ __('admin.editProcess') }}
+                        </a>
+
+                    </div>
 
                     <hr>
 
@@ -35,12 +44,21 @@
                     <br>
 
                     @foreach ($process->instantiatingTeams as $team)
-                        {!! $team->icon() !!} {{ $team->name }} <br>
+
+                        @if ($loop->first)
+                            <ul class="list-group">
+                        @endif
+
+                            <li class="list-group-item p-2">
+                                {!! $team->icon() !!} {{ $team->name }}
+                            </li>
+
+                        @if ($loop->last)
+                            </ul>
+                        @endif
                     @endforeach
 
-                </div>
-
-                <div class="col-12 col-md-9 order-2 order-md-1">
+                    <hr>
 
                     <div class="editor-content">
                         {!! App\safe_text_editor_content($process->details) !!}
@@ -51,5 +69,44 @@
             </div>
 
         </div>
+
+        <div class="col-12 col-md-4">
+
+            <div class="card mt-3 mt-md-auto">
+
+                <div class="card-body">
+
+                    <h2 class="h4"><span class="fas fa-list-ol"></span> Steps</h2>
+
+                    <hr>
+
+                    @foreach (['Self Evaluation', 'Manager Feedback', 'Executive Review'] as $stepName)
+
+
+                        <div class="mb-3">
+                            <h4 class="h5">
+                                <span class="far fa{{---check--}}-square"></span>
+                                {{ $stepName }}
+                            </h4>
+                            <p class="text-muted mb-0">
+                                <span class="fas fa-file-import"></span> {{ rand(1,3) }} Deliverables
+                                <span class="fas fa-check-square"></span> {{ rand(3, 9) }} Tasks
+                            </p>
+                        </div>
+
+                        <hr>
+
+                    @endforeach
+
+                    <div class="text-right">
+                        <a href="#" class="btn btn-sm btn-primary">
+                            <span class="fas fa-plus"></span> Add Step
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
     </div>
 @endsection
