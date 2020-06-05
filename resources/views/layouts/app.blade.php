362d33d162b8ba38fdaf27e7b62d0af3ac1b01f1
Noah Heck <noah@noahheck.com>
2020-06-05 15:58:52 -0600

    Add logo and favicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,6 +44,29 @@ $__fileTypesToCreate = $__fileTypes->filter(function($fileType) use ($_user) {
 
     <title>@yield('title', config('app.name', 'Office Forge'))</title>
 
+    <!-- ****** faviconit.com favicons ****** -->
+    <link rel="shortcut icon" href="/favicon/favicon.ico">
+    <link rel="icon" sizes="16x16 32x32 64x64" href="/favicon/favicon.ico">
+    <link rel="icon" type="image/png" sizes="196x196" href="/favicon/favicon-192.png">
+    <link rel="icon" type="image/png" sizes="160x160" href="/favicon/favicon-160.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96.png">
+    <link rel="icon" type="image/png" sizes="64x64" href="/favicon/favicon-64.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16.png">
+    <link rel="apple-touch-icon" href="/favicon/favicon-57.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/favicon/favicon-114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/favicon/favicon-72.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/favicon/favicon-144.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/favicon/favicon-60.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/favicon/favicon-120.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/favicon/favicon-76.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/favicon/favicon-152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/favicon-180.png">
+    <meta name="msapplication-TileColor" content="#FFFFFF">
+    <meta name="msapplication-TileImage" content="/favicon/favicon-144.png">
+    <meta name="msapplication-config" content="/favicon/browserconfig.xml">
+    <!-- ****** faviconit.com favicons ****** -->
+
 
     <!-- Scripts -->
     @script('js/manifest.js')
@@ -80,7 +103,8 @@ $__fileTypesToCreate = $__fileTypes->filter(function($fileType) use ($_user) {
                 </button>
 
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Office Forge') }}
+                    <img class="app-logo" alt="{{ config('app.name', 'Office Forge') }}" src="{{ asset("/images/of_logo_50.png") }}">
+                    <span>{{ config('app.name', 'Office Forge') }}</span>
                 </a>
             </span>
 
