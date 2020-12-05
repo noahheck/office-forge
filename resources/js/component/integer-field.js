cf4931b263b402133e1342768d7b364b8bfce5b5
Noah Heck <noah@noahheck.com>
2020-12-04 17:24:09 -0700

    Allow numeric Form Fields to accept negative value

--- resources/js/component/integer-field.js
+++ resources/js/component/integer-field.js
@@ -12,10 +12,12 @@ let charCodes = require('Services/character-codes');
 function isCodeAllowed(code) {
 
     if (charCodes.isControlCode(code)) {
+
         return true;
     }
 
-    if ((code >= 35 && code <= 57) || (code >= 96 && code <= 105)) {
+    if ((code >= 35 && code <= 57) || (code >= 96 && code <= 105) || ([109,173].indexOf(code) !== -1)) {
+
         return true;
     }
 
@@ -33,15 +35,31 @@ $(function() {
 
         // Allow cut/copy/paste (and probably bugs...)
         if (e.ctrlKey) {
+
             return true;
         }
 
         if (charCodes.isControlCode(charCode)) {
+
             return true;
         }
 
         if (!isCodeAllowed(charCode)) {
+
             return false;
+        }
+
+        let curCursorPosition = e.target.selectionStart;
+
+        // Allow negative sign to be added at the beginning of the sequence
+        if ([109,173].indexOf(charCode) !== -1) {
+
+            if(curVal.match(/-/)) {
+
+                return false;
+            }
+
+            return curCursorPosition === 0;
         }
     });
 
