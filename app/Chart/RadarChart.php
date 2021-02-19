19056a3961f0f974503778bdd6230c75263ee31a
Noah Heck <noah@noahheck.com>
2021-02-19 14:24:54 -0700

    Remove tooltips from radar chart

--- app/Chart/RadarChart.php
+++ app/Chart/RadarChart.php
@@ -98,6 +98,9 @@ class RadarChart implements \JsonSerializable
                         'suggestedMax' => (int) $this->max,
                     ],
                 ],
+                'tooltips' => [
+                    'enabled' => false,
+                ],
             ],
             'data' => [
                 'datasets' => [[
